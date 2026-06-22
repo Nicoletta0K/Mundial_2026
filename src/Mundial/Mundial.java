cd3b402f74c4ee2605e1484948631c212df1f2ab
Kevin Matamoros <matamorosdelgadokevin22@gmail.com>
2026-06-22 16:09:32 -0600

    Se mejoro la clase Mundial
    Se modifico la clase mundial y se agregaron mas metodos

--- src/Mundial/Mundial.java
+++ src/Mundial/Mundial.java
@@ -11,6 +11,7 @@ public class Mundial {
     private Partido[] partidos = new Partido[50];
     private int cantSelecciones = 0;
     private int cantPartidos = 0;
+    
     public Mundial(String nombre, int año, String pais) {
         this.nombre = nombre;
         this.año = año;
@@ -59,49 +60,61 @@ public class Mundial {
     }
 
     public boolean eliminarPartido(Partido p) {
-
         for (int i = 0; i < cantPartidos; i++) {
             if (partidos[i].equals(p)) {
 
                 for (int j = i; j < cantPartidos - 1; j++) {
                     partidos[j] = partidos[j + 1];
                 }
-
                 cantPartidos--;
                 return true;
             }
         }
-
         return false;
     }
     public int contarPartidos() {
         return cantPartidos;
     }
-
-    public Partido[] obtenerPartidosDeSeleccion(String nombre) {
-
-        Partido[] resultado = new Partido[50];
-        int cont = 0;
-
-        for (int i = 0; i < cantPartidos; i++) {
-
-            if (partidos[i].getLocal().getNombre().equals(nombre) ||
-                partidos[i].getVisitante().getNombre().equals(nombre)) {
-
-                resultado[cont] = partidos[i];
-                cont++;
-            }
+    public Seleccion buscarSeleccion(String nombre) {
+    for (int i = 0; i < cantSelecciones; i++) {
+        if (selecciones[i].getNombre().equals(nombre)) {
+            return selecciones[i];
         }
+    }
+    return null;
+    }
+    
+    public Partido[] obtenerPartidosDeSeleccion(String nombre) {
+    Partido[] resultado = new Partido[50];
+    int cont = 0;
+    for (int i = 0; i < cantPartidos; i++) {
+        if (partidos[i].getVisitante().getNombre().equals(nombre) ||
+            partidos[i].getLocal().getNombre().equals(nombre)) {
 
-        return resultado;
+            resultado[cont] = partidos[i];
+            cont++;
+        }
+    }
+    return resultado;
+    }
+    
+    public Seleccion[] obtenerSeleccionesGrupo(String grupo) {
+    Seleccion[] resultado = new Seleccion[50];
+    int cont = 0;
+
+    for (int i = 0; i < cantSelecciones; i++) {
+        if (selecciones[i].getGrupo().equals(grupo)) {
+            resultado[cont] = selecciones[i];
+            cont++;
+        }
+    }
+
+    return resultado;
     }
     public Partido[] buscarPartidosPorFecha(Date fecha) {
-
         Partido[] resultado = new Partido[50];
         int cont = 0;
-
         for (int i = 0; i < cantPartidos; i++) {
-
             if (partidos[i].getFecha().equals(fecha)) {
                 resultado[cont] = partidos[i];
                 cont++;
