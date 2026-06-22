2546cc6b8baaf6dea7e31b8926ce2ecaf75c954c
Luna <carlosguzmanluna8@gmail.com>
2026-06-22 14:48:06 -0600

    se modifica arbitro
    se agrega la clase arbitro que hereda de persona

--- src/Arbitros/Arbitro.java
+++ src/Arbitros/Arbitro.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Arbitros;
 
-/**
- *
- * @author norki
- */
-public class Arbitro {
-    
+import personas.Persona;
+import java.time.LocalDate;
+
+public class Arbitro extends Persona {
+    private String categoria;
+
+    public Arbitro(String nombre, String nacionalidad, LocalDate fechaNacimiento, String categoria) {
+        super(nombre, nacionalidad, fechaNacimiento);
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() { 
+        return categoria; }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria; }
 }
